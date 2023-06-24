8460995e190ce2eb4b53da82b14680ffdf20f08c
iurii_gusev <gusevyuv@gmail.com>
2023-06-24 12:38:45 -0400

    HW_objects_and_classes was made

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        Author asimov = new Author("Isaak", "Asimov");
+        Book academy = new Book("Academy", 1966, asimov);
+        academy.setPublishYear(1970);
+        System.out.println("Название книги = " + academy.getBookName());
+        System.out.println("Год издания = " + academy.getPublishYear());
+        System.out.println("Автор книги = " + academy.getAuthor().getFirstNameAuthor() +
+                " " + academy.getAuthor().getSecondNameAuthor());
 
-        Book Academy = new Book("academy", 1966, "isaak asimov");
-        Academy.setPublishYear(1970);
-        System.out.println("Название книги = " + Academy.getBookName());
-        System.out.println("Год издания = " + Academy.getPublishYear());
-        System.out.println("Автор книги = " + Academy.getAuthor().getFirstNameAuthor() + Academy.getAuthor().getSecondNameAuthor());
-
-        Book StarshipTroopers = new Book("starship troopers", 1959, "robert heinlein");
-        StarshipTroopers.setPublishYear(1965);
-        System.out.println(StarshipTroopers.getBookName() + "; " + StarshipTroopers.getPublishYear() + "; "
-                + StarshipTroopers.getAuthor().getFirstNameAuthor() +  StarshipTroopers.getAuthor().getSecondNameAuthor());
+        Author heinlein = new Author("Robert", "Heinlein");
+        Book starshipTroopers = new Book("Starship Troopers", 1959, heinlein);
+        starshipTroopers.setPublishYear(1965);
+        System.out.println(starshipTroopers.getBookName() + "; " + starshipTroopers.getPublishYear() + "; "
+                + starshipTroopers.getAuthor().getFirstNameAuthor() + " " + starshipTroopers.getAuthor().getSecondNameAuthor());
     }
 }
